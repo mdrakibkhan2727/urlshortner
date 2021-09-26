596f254a683b36cb5525518620f9c698c1005da3
Faisal Islam <faisal.islam@vivacomsolutions.com>
2021-09-26 14:56:32 +0600

    Code refactored

--- _source_/app/Http/Controllers/ShortLinkController.php
+++ _source_/app/Http/Controllers/ShortLinkController.php
@@ -5,18 +5,19 @@ namespace App\Http\Controllers;
 use App\Action\UrlShortenActions;
 use App\Http\Requests\ShortlinkRequest;
 use App\Models\ShortLink;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 
 class ShortLinkController extends Controller
 {
-    public function index(UrlShortenActions $shortenActions)
+    public function index(UrlShortenActions $shortenActions): View
     {
         return view('short-link.index', [
             'shortLinks' => $shortenActions->findAll()
         ]);
     }
 
-    public function store(ShortlinkRequest $request, UrlShortenActions $shortenActions)
+    public function store(ShortlinkRequest $request, UrlShortenActions $shortenActions): RedirectResponse
     {
         $shortenActions->create( $request->input('link') );
 
